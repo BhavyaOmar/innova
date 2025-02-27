5ce538bd11ee87a34fb261de3adb3453169031df
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2025-02-27 17:14:56 +0530

    UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { Children } from "react";
 import Welcome from "./Pages/Welcome";
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+
+      <div className="my-32">
+        <h1 className="mx-8 text-3xl">Home Page</h1>
+      </div>
+
+      <Footer/>
     </div>
   );
 };
