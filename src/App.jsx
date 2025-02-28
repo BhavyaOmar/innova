e743d836f8ab06ed7c055a35bbb6ab069c6a26d4
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2025-02-28 09:03:09 +0530

    News

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,6 @@ import Welcome from "./Pages/Welcome";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./Pages/Dashboard";
 import Footer from "./Components/Footer";
-<<<<<<< HEAD
-import NavbarWelcome from "./Components/NavbarWelcome";
-import Login from "./Pages/Login";
-import Signup from "./Pages/SignUp";
-
-const App = () => {
-  return (
-    <Router>
-      <NavbarWelcome/>
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/discover" element={<Discover />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="*" element={<h1>❌ 404 - Page Not Found</h1>} />
-      </Routes>
-      <Footer />
-    </Router>
-=======
 import Error404 from "./Pages/Error404";
 import CompanyInfo from "./Pages/CompanyInfo";
 import { FinanceProvider } from "./Contexts/FInanceProvider"; // Corrected import
@@ -44,7 +24,6 @@ const App = () => {
         <Footer />
       </Router>
     </FinanceProvider>
->>>>>>> upstream/main
   );
 };
 
